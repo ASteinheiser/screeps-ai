a5602e622f51b87bd4c5dd5460f13cd60b8ba4d2
Andrew Steinheiser <me@iamandrew.io>
2021-09-27 00:23:06 -0700

    tower placeholder

---
+++ src/structures/tower.ts
@@ -0,0 +1,14 @@
+// var tower = Game.getObjectById('8c8a5a5980394d080c83fca3');
+// if(tower) {
+//   var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+//     filter: (structure) => structure.hits < structure.hitsMax
+//   });
+//   if(closestDamagedStructure) {
+//     tower.repair(closestDamagedStructure);
+//   }
+
+//   var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+//   if(closestHostile) {
+//     tower.attack(closestHostile);
+//   }
+// }
