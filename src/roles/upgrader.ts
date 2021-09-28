d45c5a42f1448a80b48463ed191a8658cb55fdaa
Andrew Steinheiser <me@iamandrew.io>
2021-09-28 01:44:49 -0700

    refactor upgrader

--- src/roles/upgrader.ts
+++ src/roles/upgrader.ts
@@ -1,25 +1,27 @@
+import { findResource } from './harvester';
+
 const PATH_COLOR = '#ffaa00';
 
 export const upgrade = (creep: Creep) => {
   if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
     creep.memory.working = false;
-    creep.say('🔄 harvest');
+    creep.say('⛏️ harvest');
   }
-
   if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
     creep.memory.working = true;
     creep.say('⚡ upgrade');
   }
 
-  if (creep.memory.working && creep.room.controller) {
-    if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: PATH_COLOR}});
-    }
+  if (creep.memory.working) {
+    upgradeController(creep);
+  } else {
+    findResource(creep);
   }
-  else {
-    const sources = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(sources[0], {visualizePathStyle: {stroke: PATH_COLOR}});
-    }
+}
+
+const upgradeController = (creep: Creep) => {
+  if (!creep.room.controller) return; // TODO: make unit harvest or something?
+  if (creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: PATH_COLOR}});
   }
 }
