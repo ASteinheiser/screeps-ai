6eecaa274377b122a96dd3def6623312f903705d
Andrew Steinheiser <me@iamandrew.io>
2021-09-28 01:45:07 -0700

    refactor builder

--- src/roles/builder.ts
+++ src/roles/builder.ts
@@ -1,27 +1,27 @@
+import { findResource } from './harvester';
+
 const PATH_COLOR = '#ffaa00';
 
 export const build = (creep: Creep) => {
   if (creep.memory.working && creep.store[RESOURCE_ENERGY] === 0) {
     creep.memory.working = false;
-    creep.say('🔄 harvest');
+    creep.say('⛏️ harvest');
   }
-
   if (!creep.memory.working && creep.store.getFreeCapacity() === 0) {
     creep.memory.working = true;
     creep.say('🚧 build');
   }
 
   if (creep.memory.working) {
-    const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-    if (targets.length) {
-      if (creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0], {visualizePathStyle: {stroke: PATH_COLOR}});
-      }
-    }
+    buildStructure(creep);
   } else {
-    const sources = creep.room.find(FIND_SOURCES);
-    if (creep.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
-      creep.moveTo(sources[0], {visualizePathStyle: {stroke: PATH_COLOR}});
-    }
+    findResource(creep);
+  }
+}
+
+const buildStructure = (creep: Creep) => {
+  const targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+  if (targets.length && creep.build(targets[0]) === ERR_NOT_IN_RANGE) {
+    creep.moveTo(targets[0], {visualizePathStyle: {stroke: PATH_COLOR}});
   }
 }
