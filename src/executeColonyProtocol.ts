4093bda93868c28d470dc4147307ed8ac3f438c9
Andrew Steinheiser <me@iamandrew.io>
2021-09-29 23:29:26 -0700

    execute protocols

--- src/executeColonyProtocol.ts
+++ src/executeColonyProtocol.ts
@@ -1,15 +1,16 @@
+import { executeProtocols } from './protocol';
 import assignRoles from './assignRoles';
 import autoSpawn from './autoSpawn';
-import { Role } from './types';
 
 const executeColonyProtocol = () => {
   console.log('executing colony protocols...');
 
-  assignRoles();
-
-  autoSpawn({ spawnName: 'Spawn1', role: Role.harvester, max: 2 });
-  autoSpawn({ spawnName: 'Spawn1', role: Role.builder, max: 2 });
-  autoSpawn({ spawnName: 'Spawn1', role: Role.upgrader, max: 2 });
+  executeProtocols([
+    assignRoles,
+    () => autoSpawn({ spawnName: 'Spawn1', role: 'harvester', max: 2 }),
+    () => autoSpawn({ spawnName: 'Spawn1', role: 'builder', max: 2 }),
+    () => autoSpawn({ spawnName: 'Spawn1', role: 'upgrader', max: 2 }),
+  ]);
 }
 
 export default executeColonyProtocol;
