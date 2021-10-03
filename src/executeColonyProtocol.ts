7aafa069519f5d72799b73e22baee5931713aac1
Andrew Steinheiser <me@iamandrew.io>
2021-10-03 03:36:32 -0700

    run two sets of execture protocols (to enable running certain ones every tick)

--- src/executeColonyProtocol.ts
+++ src/executeColonyProtocol.ts
@@ -6,14 +6,19 @@ import showSpawnMessages from './protocols/showSpawnMessages';
 const executeColonyProtocol = () => {
   console.log('executing colony protocols...');
 
+  // protocols for spawning colony creeps
   executeProtocols([
-    assignRoles,
     () => autoSpawn({ spawnName: 'Spawn1', role: 'harvester', max: 2 }),
     () => autoSpawn({ spawnName: 'Spawn1', role: 'builder', max: 2 }),
     () => autoSpawn({ spawnName: 'Spawn1', role: 'upgrader', max: 2 }),
     () => autoSpawn({ spawnName: 'Spawn1', role: 'harvester', max: 4 }),
     () => autoSpawn({ spawnName: 'Spawn1', role: 'builder', max: 4 }),
     () => autoSpawn({ spawnName: 'Spawn1', role: 'upgrader', max: 4 }),
+  ]);
+
+  // protocols that need to be called every tick (always return `true`)
+  executeProtocols([
+    assignRoles,
     showSpawnMessages,
   ]);
 }
