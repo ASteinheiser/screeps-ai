c005d06c4198e78f7f4c82e8bbbee672f91fe47c
Andrew Steinheiser <me@iamandrew.io>
2021-10-03 15:03:46 -0700

    better typing around Protocol type functions

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,4 +1,4 @@
-export type Protocol = (args?: any) => boolean;
+export type Protocol<T = void> = (args: T) => boolean;
 
 export const executeProtocols = (protocols: Protocol[]) => {
   const protocol = protocols.shift();
