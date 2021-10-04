d445e7e9425ff11f3343a3464b59300f366a7a10
Andrew Steinheiser <me@iamandrew.io>
2021-10-04 00:42:00 -0700

    really unfortunate workaround to not being able to load @types/screeps in mocha tests

--- test/setup-mocha.js
+++ test/setup-mocha.js
@@ -5,5 +5,16 @@ global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
 
+// TODO: literally anything to avoid this... can't figure out how
+// to get mocha to get these constants out of `@types/screeps`
+global.WORK = 'work';
+global.CARRY = 'carry';
+global.MOVE = 'move';
+global.BODYPART_COST = {
+  [WORK]: 100,
+  [CARRY]: 50,
+  [MOVE]: 50,
+};
+
 // Override ts-node compiler options
 process.env.TS_NODE_PROJECT = 'tsconfig.test.json'
