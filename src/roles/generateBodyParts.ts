0e342dafc61bf45aee363ad4fbfafb3516bba2af
Andrew Steinheiser <me@iamandrew.io>
2021-10-03 23:25:49 -0700

    generate the worker body parts based on max energy available per controller level

--- src/roles/generateBodyParts.ts
+++ src/roles/generateBodyParts.ts
@@ -1,7 +1,44 @@
 import { Role, MAX_CREEP_COST } from './constants';
 
+const generateParts = (part: BodyPartConstant, count: number) => {
+  return [...Array(count)].map(() => part);
+}
+
+const generateWorkerParts = (controllerLevel: number) => {
+  const maxEnergy = MAX_CREEP_COST[controllerLevel];
+
+  // make workers have work parts equal to at least half of the max energy
+  const workParts = Math.ceil((maxEnergy / 2 ) / BODYPART_COST[WORK]);
+  const workPartsCost = workParts * BODYPART_COST[WORK];
+
+  let carryParts = 1;
+  let moveParts = 1;
+  const remainingEnergy = maxEnergy - workPartsCost;
+  if (remainingEnergy > 100) {
+    // make workers have move parts equal to 60% of the remaining energy
+    moveParts = Math.ceil((remainingEnergy * .6) / BODYPART_COST[MOVE]);
+    const movePartsCost = moveParts * BODYPART_COST[MOVE];
+
+    const carryPartsBudget = remainingEnergy - movePartsCost;
+    carryParts = Math.floor(carryPartsBudget / BODYPART_COST[CARRY]);
+  }
+
+  return [
+    ...generateParts(WORK, workParts),
+    ...generateParts(CARRY, carryParts),
+    ...generateParts(MOVE, moveParts)
+  ];
+}
+
 export const generateBodyParts = (controllerLevel: number, role: Role) => {
-  return [];
+  switch(role) {
+    case Role.harvester:
+    case Role.builder:
+    case Role.upgrader:
+      return generateWorkerParts(controllerLevel);
+    default:
+      return [WORK, CARRY, MOVE];
+  }
 }
 
 const generatePartsPerControllerLevel = (role: Role) => {
