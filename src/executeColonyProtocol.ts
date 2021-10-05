3dcc0b2cb950892009f60106c54c95cf4c710164
Andrew Steinheiser <me@iamandrew.io>
2021-10-05 10:33:03 -0700

    make towers do their action, cheap solution for now

--- src/executeColonyProtocol.ts
+++ src/executeColonyProtocol.ts
@@ -1,6 +1,7 @@
 import { executeProtocols } from './protocol';
 import { assignRoles, autoSpawn, showSpawnMessages } from './protocols';
 import { Role } from './roles';
+import { towerActions } from './structures';
 
 const executeColonyProtocol = () => {
   console.log('executing colony protocols...');
@@ -18,6 +19,11 @@ const executeColonyProtocol = () => {
       () => autoSpawn({ spawnName, role: Role.builder, max: 3 }),
       () => autoSpawn({ spawnName, role: Role.upgrader, max: 3 }),
     ]);
+  }
+
+  for (const roomName in Game.rooms) {
+    const towers = Game.rooms[roomName].find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
+    towers.forEach(tower => towerActions(tower as StructureTower));
   }
 
   // protocols that need to be called every tick (always return `true`)
