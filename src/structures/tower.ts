cf0b59da50ffd847b8272fa6b5286bd47c27b443
Andrew Steinheiser <me@iamandrew.io>
2021-10-05 10:32:47 -0700

    basic tower actions

--- src/structures/tower.ts
+++ src/structures/tower.ts
@@ -1,14 +1,15 @@
-// var tower = Game.getObjectById('8c8a5a5980394d080c83fca3');
-// if(tower) {
-//   var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-//     filter: (structure) => structure.hits < structure.hitsMax
-//   });
-//   if(closestDamagedStructure) {
-//     tower.repair(closestDamagedStructure);
-//   }
+export const towerActions = (tower: StructureTower) => {
+  const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+  if (closestHostile) {
+    tower.attack(closestHostile);
+    return;
+  }
 
-//   var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-//   if(closestHostile) {
-//     tower.attack(closestHostile);
-//   }
-// }
+  const closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+    filter: (structure) => structure.hits < structure.hitsMax
+  });
+  if (closestDamagedStructure) {
+    tower.repair(closestDamagedStructure);
+    return;
+  }
+}
