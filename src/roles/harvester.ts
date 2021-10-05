4691924de2c52850bbcdb9ee0ef740e20263ebb8
Andrew Steinheiser <me@iamandrew.io>
2021-10-05 10:17:53 -0700

    fill up towers with energy first

--- src/roles/harvester.ts
+++ src/roles/harvester.ts
@@ -57,9 +57,9 @@ export const findTargetsForDeposit = (creep: Creep) => {
   // Deposit energy in structures with the following order:
   // extensions -> spawn -> towers -> containers -> anything else
   return _.sortBy(structures, ({ structureType }) => {
-    if (structureType === STRUCTURE_EXTENSION) return 0;
-    if (structureType === STRUCTURE_SPAWN) return 1;
-    if (structureType === STRUCTURE_TOWER) return 2;
+    if (structureType === STRUCTURE_TOWER) return 0;
+    if (structureType === STRUCTURE_EXTENSION) return 1;
+    if (structureType === STRUCTURE_SPAWN) return 2;
     if (structureType === STRUCTURE_CONTAINER) return 3;
     return 4;
   });
